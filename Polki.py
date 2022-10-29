511a03f93d3f3fee8a8bef6469bc9ce17f3b57d8
kruk97 <bartekandrzejtkaczyk@gmail.com>
2022-10-29 16:34:23 +0200

    faza pierwsza

--- Polki.py
+++ Polki.py
@@ -1,43 +1,40 @@
-import requests
-import selenium
-import time 
-	
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options  
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-from bs4 import BeautifulSoup
-
-
-chrome_options = Options()
-chrome_options.add_argument("--headless") 
-driver = webdriver.Chrome(chrome_options=chrome_options)
 
 page = 'https://polki.pl/po-godzinach/z-zycia-wziete,.html'
 rejectCookiesXPATH = "//button[@class='tcf277-button tcf277-button--slim accept-targeting-disclaimer-button']"
 positionOfLinks = "//div[@class='row list-box small-up-1 medium-up-3']"
+webdriver_path = 'chromedriver_linux64/chromedriver'
+
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(executable_path=webdriver_path, options=chrome_options)
+driver.get(page)
+
 
 def delete_cookies():
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, rejectCookiesXPATH)))
+    driver.find_element(By.XPATH, rejectCookiesXPATH).click()
 
-	driver.get(page)
-	
-	WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, rejectCookiesXPATH)))
-	driver.find_element(By.XPATH, rejectCookiesXPATH).click()	
-	
+
 def get_links():
-	
-	
-	el = driver.find_element(By.XPATH, positionOfLinks)
-	if el:
-		url = el.get_attribute('href')
-		if url is not None:
-			for i in url:
-				print(url)
+    div_list_with_links = driver.find_elements(By.XPATH, positionOfLinks)
+    links = []
 
-		else:
-			print('dupa')
+    for div in div_list_with_links:
+        links.extend(div.find_elements(By.CSS_SELECTOR, 'div[class=text-box] a'))
+
+    res = []
+    for link in links:
+        res.append(link.get_attribute('href'))
+    return res
+
+def get_article(links):
+    pass
 
 delete_cookies()
-
-get_links()
+article_links = get_links()
+get_article(article_links)
